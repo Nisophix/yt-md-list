179c11dc17e007f19405aac576011336f8a4f6e5
Nisophix <89792349+Nisophix@users.noreply.github.com>
2024-05-14 23:06:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,5 @@ sys.stdout = open(path, 'w')
 
 videos = scrapetube.get_channel(channel_id)
 
-print(videos)
-
 for video in videos:
     print("- [ ] ["+str(video['title']['runs'][0]['text'])+"](https://www.youtube.com/watch?v="+str(video['videoId'])+")")
